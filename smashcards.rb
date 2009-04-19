3f1fdedaac933348e117ea384e69d1f668ce03fb
phren0logy <andrew.nanton@lobesandrobes.com>
2009-04-19 11:53:15 -0400

    A bit fancier, with some changing backgrounds.

--- smashcards.rb
+++ smashcards.rb
@@ -13,26 +13,42 @@ end
 require 'csv'
 
 Shoes.app do
+  def circles
+    fill rgb(0, 0.6, 0.9, 0.1)
+    stroke rgb(0, 0.6, 0.9)
+    strokewidth 0.25
+    100.times {
+      oval :left => (-5..self.width).rand,
+        :top => (-5..self.height).rand,
+        :radius => (25..50).rand
+      }
+  end
+  
+  def stars
+    fill rgb(255, 215, 0, 0.1)
+    stroke goldenrod
+    strokewidth 0.25
+    50.times {
+      star :left => (-5..self.width).rand,
+        :top => (-5..self.height).rand,
+        :points => (5..8).rand
+      }
+  end
+  
   flow do
     # Title at the top
     stack :height => 50 do
       background green 
       title "SmashCards" 
     end
+    
     # This should be replaced with the flash cards
     @main_window = stack :width => 600, :height => 300, :margin => 20 do
-        fill rgb(0, 0.6, 0.9, 0.1)
-        stroke rgb(0, 0.6, 0.9)
-        strokewidth 0.25
-        100.times {
-          oval :left => (-5..self.width).rand,
-            :top => (-5..self.height).rand,
-            :radius => (25..50).rand
-          }
-        @the_text = para "You can open a flashcard file here, by chosing open below. ",
-        "You will need a file in csv (or comma separated value) format, ",
-        "with one line per flashcard, and the front and back separated "
-        "by a comma."
+      circles
+      @the_text = para "You can open a flashcard file here, by chosing open below. ",
+      "You will need a file in csv (or comma separated value) format, ",
+      "with one line per flashcard, and the front and back separated "
+      "by a comma."
     end
     
     @open_stack = stack :height => 30, :width => 600 do
@@ -48,23 +64,38 @@ Shoes.app do
     end
 
     @card_navigator_stack = flow :height => 30, :width => 600 do
+      def change_background
+        if @side == 1
+          @main_window.clear {
+            stars
+            @the_text = para @data[@card][@side]
+            @the_text.align = 'center'
+            }
+        else
+          @main_window.clear {
+            circles
+            @the_text = para @data[@card][@side]
+            @the_text.align = 'center'
+          }
+        end
+      end
+      
+      
+      
           @flip_button = button("flip") do
             @side == 1 ? @side = 0 : @side = 1
-            #@main_window.background limegreen
-            @the_text.replace @data[@card][@side]
+            change_background
           end
           
           @next_button = button("next") do
             @card < (@data.length-1) ? @card += 1 : @card = @data.length-1
-            @side = 0
-            #@the_text.replace @card
-            @the_text.replace @data[@card][@side]
+            change_background
           end
           
           @back_button = button("back") do
             @card > 1 ? @card -=1 : @card = 0
             @side = 0
-            @the_text.replace @data[@card][@side]
+            change_background
           end
     end
   end
